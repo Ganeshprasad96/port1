adda1981b730c21a180b4cbac614be7efaf6b235
tusharsaliann <tusharsalian89@gmail.com>
2024-11-09 09:47:12 +0530

    first commit

--- src/Data/PersonalData.js
+++ src/Data/PersonalData.js
@@ -6,7 +6,7 @@ const PersonalData = {
     email:"ganeshbiliyoor020@gmail.com",
     address:"India",
     aboutMe:"Hi Everyone, I am Ganesh Prasad from Karnataka, India.I learned Python, Html, CSS, JavaScript etc, Worked with libraries & frameworks like ReactJS. Have a keen knowledge of databases like MySQL, MongoDB. Eager to contribute to innovative projects and gain hands-on experience in a dynamic and collaborative",
-    resumeLink:'https://drive.google.com/file/d/1vfnzDcN4uATaNZ7cc9kdKvep9ZBUN_be/view?usp=sharing',
+    resumeLink:'',
     imageUrl:''
 }
 export const autoTypeData=['3rd Year CSE', 'Full Stack Developer','Java Developer', 'Open Source Contributor'];
--- src/Data/ProjectsData.js
+++ src/Data/ProjectsData.js
@@ -2,19 +2,19 @@ const DUMMY_PROJECTS=[
     {
         projectTitle:'PortFolio Template',
         description: 'This project is a React app to generate a creative professional website based on Data provided. The website uses no backend and is free to fork and use.',
-        sourceLink:'https://github.com/killer-abhi/Portfolio.git',
+        sourceLink:'',
         lastUpdated:'Jan 20 2023'
     },
     {
         projectTitle:'Temple Website',
         description: "Temple website using HTML and CSS that provides information about daily prayers, rituals, and festivals. It features a clean layout to showcase temple traditions and events",
-        sourceLink:'https://ganeshprasad18.github.io/ganesh2/project',
+        sourceLink:'https://ganeshprasad96.github.io/temple3/',
         lastUpdated:'Dec 30 2024'
     },
     {
         projectTitle:'Calculator',
         description: 'It is a Realtime Chat Website built using Socket.io and Express with NodeJs as a backend and MongoDB as a database. It updates the chat messages in realtime with Typing notification of other users',
-        sourceLink:'https://github.com/killer-abhi/realtime-chat-site.git',
+        sourceLink:'',
         lastUpdated:'Dec 10 2022'
     },
     
